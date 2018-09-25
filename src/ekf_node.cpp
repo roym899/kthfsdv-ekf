99f8fb4203dae1897e19577a56fa3a49ee18645d
Leonard Bruns <leonardb@kth.se>
2018-09-25 22:58:58 +0200

    Add EKF formulas

--- src/ekf_node.cpp
+++ src/ekf_node.cpp
@@ -5,28 +5,120 @@
 
 #include <sstream>
 #include <cmath>
+#include <Eigen/Dense>
+
+using namespace Eigen;
 
 const double pi = 3.1415;
 
 const int ekf_frequency = 100;
 
-double current_x = 0;
-double current_y = 0;
-double current_theta = 0;
+ros::Time last_update_time;
+bool first_update = true;
+
+Matrix<float, 4, 1> current_state = Matrix<float, 4, 1>::Zero(); // x, y, theta, v
+Matrix<float, 4, 4> current_covariance = Matrix<float, 4, 4>::Zero();
+
+void init_ekf()
+{
+  current_covariance(0,0) = 1;
+  current_covariance(1,1) = 1;
+  current_covariance(2,2) = 1;
+  current_covariance(3,3) = 1;
+}
 
 void imuCallback(const sensor_msgs::Imu::ConstPtr& imu_msg)
 {
+  double dt;
+  if(first_update) {
+    last_update_time = imu_msg->header.stamp;
+    first_update = false;
+    return;
+  }
+  else {
+    dt = (imu_msg->header.stamp - last_update_time).toSec();
+    last_update_time = imu_msg->header.stamp;
+  }
   ROS_DEBUG("Imu data received");
+  Matrix<float, 2, 2> Q = Matrix<float, 2, 2>::Zero();
+  Q(0,0) = 0.5;
+  Q(1,1) = 0.1;
+
+  current_state(0) = current_state(0) + current_state(3)*cos(current_state(2))*dt;
+  current_state(1) = current_state(1) + current_state(3)*sin(current_state(2))*dt;
+  current_state(2) = current_state(2) + imu_msg->angular_velocity.z*dt;
+  current_state(3) = current_state(3) + imu_msg->linear_acceleration.x*dt;
+
+  Matrix<float, 4, 4> grad_f_x = Matrix<float,4,4>::Zero();
+
+  grad_f_x(0,0) = 1;
+  grad_f_x(0,2) = -current_state(3)*sin(current_state(2))*dt;
+  grad_f_x(0,3) = cos(current_state(2))*dt;
+  grad_f_x(1,1) = 1;
+  grad_f_x(1,2) = current_state(3)*cos(current_state(2))*dt;
+  grad_f_x(1,3) = sin(current_state(2))*dt;
+  grad_f_x(2,2) = 1;
+  grad_f_x(3,3) = 1;
+
+  Matrix<float,4,2> grad_f_v = Matrix<float,4,2>::Zero();
+  grad_f_v(2,0) = -dt;
+  grad_f_v(3,1) = dt;
+  current_covariance = grad_f_x*current_covariance*grad_f_x.transpose()+grad_f_v*Q*grad_f_v.transpose();
+
 }
 
 void gpsCallback(const geometry_msgs::Pose2D::ConstPtr& gps_msg)
 {
-  ROS_DEBUG("GPS data received");
+  Matrix<float, 2, 2> R = Matrix<float, 2, 2>::Zero();
+  R(0,0) = 2;
+  R(1,1) = 2;
+
+  Matrix<float, 2, 1> h;
+  h(0) = current_state(0);
+  h(1) = current_state(1);
+
+  Matrix<float, 2, 1> z;
+  z(0) = gps_msg->x;
+  z(1) = gps_msg->y;
+
+  Matrix<float, 2, 4> grad_h_x = Matrix<float, 2, 4>::Zero();
+  grad_h_x(0, 0) = 1;
+  grad_h_x(1, 1) = 1;
+
+  Matrix<float, 2, 2> grad_h_w = Matrix<float, 2, 2>::Zero();
+  grad_h_w(0, 0) = 1;
+  grad_h_w(1, 1) = 1;
+
+  Matrix<float, 2, 2> S = grad_h_w*R*grad_h_w.transpose() + grad_h_x*current_covariance*grad_h_x.transpose();
+  Matrix<float, 4, 2> W = current_covariance*grad_h_x.transpose()*S.inverse();
+
+  current_state = current_state+(W*(z-h));
+  current_covariance = current_covariance-W*S*W.transpose();
+
 }
 
 void speedometerCallback(const std_msgs::Float64::ConstPtr& speedometer_msg)
 {
-  ROS_DEBUG("Speedometer data received");
+  Matrix<float, 1, 1> R;
+  R(0,0) = 0.01;
+
+  Matrix<float, 1, 1> h;
+  h(0) = current_state(3);
+
+  Matrix<float, 1, 1> z;
+  z(0) = speedometer_msg->data;
+
+  Matrix<float, 1, 4> grad_h_x = Matrix<float, 1, 4>::Zero();
+  grad_h_x(0, 3) = 1;
+
+  Matrix<float, 1, 1> grad_h_w;
+  grad_h_w(0, 0) = 1;
+
+  Matrix<float, 1, 1> S = grad_h_w*R*grad_h_w.transpose() + grad_h_x*current_covariance*grad_h_x.transpose();
+  Matrix<float, 4, 1> W = current_covariance*grad_h_x.transpose()*S.inverse();
+
+  current_state = current_state+(W*(z-h));
+  current_covariance = current_covariance-W*S*W.transpose();
 }
 
 int main(int argc, char **argv)
@@ -42,16 +134,19 @@ int main(int argc, char **argv)
 
   ros::Rate loop_rate(ekf_frequency);
 
+  init_ekf();
+
   while (ros::ok())
   {
     // filter update will be handled inside callbacks
     ros::spinOnce();
+      ROS_INFO_STREAM(current_state);
 
     // publish the current estimate
     geometry_msgs::Pose2D estimate_msg;
-    estimate_msg.x = current_x;
-    estimate_msg.y = current_y;
-    estimate_msg.theta = current_theta;
+    estimate_msg.x = current_state(0);
+    estimate_msg.y = current_state(1);
+    estimate_msg.theta = current_state(2);
     estimate_pub.publish(estimate_msg);
 
     loop_rate.sleep();
